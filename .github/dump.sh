689b142a780cac68ac1d7da7d79fdad60649b66e
Matus Novak <email@matusnovak.com>
2021-07-11 07:34:10 +0000

    Fix dump logs and install jmespath

---
+++ .github/dump.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p /tmp/microk8s
+kubectl get all --all-namespaces 2>&1 > /tmp/microk8s/get-all.log
+for type in pod service deployment statefulset replicaset daemonset pvc
+do
+  mkdir -p "/tmp/microk8s/$type"
+
+  items=$(kubectl get $type --all-namespaces --template "{{range .items}}{{.metadata.namespace}}/{{.metadata.name}} {{end}}")
+  
+  for item in $items
+  do
+    echo "Checking: $type - $item"
+    name="$(cut -d'/' -f2 <<< $item)"
+    namespace="$(cut -d'/' -f1 <<< $item)"
+
+    kubectl describe $type --namespace $namespace $name 2>&1 > "/tmp/microk8s/$type/$namespace-$name.log"
+
+    if [ $type = "pod" ]; then
+      containers=$(kubectl get pod --namespace $namespace $name -o "jsonpath={.spec.containers[*].name}")
+      for container in $containers
+      do
+        kubectl logs --namespace $namespace $name -c $container 2>&1 > "/tmp/microk8s/logs/$namespace-$name-$container.log"
+      done
+    fi
+  done
+done
+
+mkdir -p "/tmp/microk8s/pv"
+names=$(kubectl get pv --all-namespaces --template "{{range .items}}{{.metadata.name}} {{end}}")
+for name in $names
+do
+  echo "Checking: pv - $name"
+  kubectl describe pv $name 2>&1 > "/tmp/microk8s/pv/$name.log"
+done
